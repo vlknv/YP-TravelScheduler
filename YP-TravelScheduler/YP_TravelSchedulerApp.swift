566736355502fa25aeeb7f5eec4e5036211cba83
Aleksandr Velikanov <velikanov.atwork@gmail.com>
2024-03-02 17:52:46 +0300

    Initial Commit

---
+++ YP-TravelScheduler/YP_TravelSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YP_TravelSchedulerApp.swift
+//  YP-TravelScheduler
+//
+//  Created by Aleksandr Velikanov on 02.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct YP_TravelSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
