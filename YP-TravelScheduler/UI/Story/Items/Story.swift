993ef47ccf247c450ef10e2efb5a8d7f69980636
Aleksandr Velikanov <velikanov.atwork@gmail.com>
2024-06-12 15:51:46 +0300

    #7 - Добавлены ресурсы

--- YP-TravelScheduler/UI/Story/Items/Story.swift
+++ YP-TravelScheduler/UI/Story/Items/Story.swift
@@ -40,53 +40,93 @@ extension Story {
     static var mockStories: [Story] = [
         .init(
             id: UUID(),
-            date: Date.now.addingTimeInterval(4),
-            miniatureImage: .storyMiniature1,
-            fullscreenImage: .storyFull1,
+            date: Date.now.addingTimeInterval(8),
+            miniatureImage: .storyPreview1,
+            fullscreenImage: .storyFull11,
             title: "Text1",
-            details: "Text Text1 Text1 Text1",
+            details: "Text1 Text1 Text1 Text1",
             isRead: false,
-            duration: 2
+            duration: 5
+        ),
+        .init(
+            id: UUID(),
+            date: Date.now.addingTimeInterval(7),
+            miniatureImage: .storyPreview2,
+            fullscreenImage: .storyFull21,
+            title: "Text2",
+            details: "Text2 Text2 Text2 Text2",
+            isRead: false,
+            duration: 5
+        ),
+        .init(
+            id: UUID(),
+            date: Date.now.addingTimeInterval(6),
+            miniatureImage: .storyPreview3,
+            fullscreenImage: .storyFull31,
+            title: "Text3",
+            details: "Text3 Text3 Text3 Text3",
+            isRead: false,
+            duration: 5
+        ),
+        .init(
+            id: UUID(),
+            date: Date.now.addingTimeInterval(5),
+            miniatureImage: .storyPreview4,
+            fullscreenImage: .storyFull41,
+            title: "Text4",
+            details: "Text4 Text4 Text4 Text4",
+            isRead: false,
+            duration: 5
+        ),
+        .init(
+            id: UUID(),
+            date: Date.now.addingTimeInterval(4),
+            miniatureImage: .storyPreview5,
+            fullscreenImage: .storyFull51,
+            title: "Text5",
+            details: "Text5 Text5 Text5 Text5",
+            isRead: false,
+            duration: 5
         ),
         .init(
             id: UUID(),
             date: Date.now.addingTimeInterval(3),
-            miniatureImage: .storyMiniature2,
-            fullscreenImage: .storyFull2,
-            title: "Text2",
-            details: "Text2 Text2 Text2 Text2",
+            miniatureImage: .storyPreview6,
+            fullscreenImage: .storyFull61,
+            title: "Text6",
+            details: "Text6 Text6 Text6 Text6",
             isRead: false,
-            duration: 2
+            duration: 5
         ),
         .init(
             id: UUID(),
             date: Date.now.addingTimeInterval(2),
-            miniatureImage: .storyMiniature3,
-            fullscreenImage: .storyFull3,
-            title: "Text3",
-            details: "Text3 Text3 Text3 Text3",
+            miniatureImage: .storyPreview7,
+            fullscreenImage: .storyFull71,
+            title: "Text7",
+            details: "Text7 Text7 Text7 Text7",
             isRead: false,
-            duration: 2
+            duration: 5
         ),
         .init(
             id: UUID(),
             date: Date.now.addingTimeInterval(1),
-            miniatureImage: .storyMiniature4,
-            fullscreenImage: .storyFull4,
-            title: "Text4",
-            details: "Text4 Text4 Text4 Text4",
+            miniatureImage: .storyPreview8,
+            fullscreenImage: .storyFull81,
+            title: "Text8",
+            details: "Text8 Text8 Text8 Text8",
             isRead: false,
-            duration: 2
+            duration: 5
         ),
         .init(
             id: UUID(),
             date: Date.now.addingTimeInterval(0),
-            miniatureImage: .storyMiniature5,
-            fullscreenImage: .storyFull5,
-            title: "Text5",
-            details: "Text5 Text5 Text5 Text5",
+            miniatureImage: .storyPreview9,
+            fullscreenImage: .storyFull91,
+            title: "Text9",
+            details: "Text9 Text9 Text9 Text9",
             isRead: false,
-            duration: 2
+            duration: 5
         ),
     ]
     .sorted(by: >)
